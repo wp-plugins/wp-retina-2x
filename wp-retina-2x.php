d91d6d759256eaf23d45acf2b5110babe4f2e3c6
TigrouMeow <TigrouMeow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-10 10:42:10 +0000

    Tag 0.2.4. Release.
    git-svn-id: https://plugins.svn.wordpress.org/wp-retina-2x/tags/0.2.4@569960 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-retina-2x.php
+++ wp-retina-2x.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Retina 2x
 Plugin URI: http://www.meow.fr/wp-retina-2x
 Description: Your website will look beautiful and smooth on Retina displays.
-Version: 0.2.2
+Version: 0.2.4
 Author: Jordy Meow
 Author URI: http://www.meow.fr
 
