ee2ecc7b2ff03e98fdbd22e0e5646e1d64a23409
TigrouMeow <TigrouMeow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-01 10:11:24 +0000

    Fixed: DEBUG was 'true' for Retina-Images.
    git-svn-id: https://plugins.svn.wordpress.org/wp-retina-2x/tags/0.3.4@593306 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wr2x_image.php
+++ wr2x_image.php
@@ -2,7 +2,7 @@
 	//require( 'wp-retina-2x.php' );
 	//$debug = wr2x_getoption( "debug", "wr2x_advanced", false );
 
-	define('DEBUG',              true);   	// Write debugging information to a log file
+	define('DEBUG',              false);   	// Write debugging information to a log file
 	define('SEND_ETAG',          true);     // You will want to disable this if you load balance multiple servers 
 	define('SEND_EXPIRES',       true);     // 
 	define('SEND_CACHE_CONTROL', true);     // 
