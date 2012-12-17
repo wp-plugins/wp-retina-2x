609c0c130c887e0f13f01f2786adcfd90d7cc74c
TigrouMeow <TigrouMeow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-17 10:57:50 +0000

    * Fix: "<?" to "<?php"
    git-svn-id: https://plugins.svn.wordpress.org/wp-retina-2x/tags/0.9@640466 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jordy_meow_footer.php
+++ jordy_meow_footer.php
@@ -5,7 +5,7 @@
 			<div style='font-size: 14px; color: #32595E; border-top: 1px solid #DFDFDF; position: absolute;margin-right: 20px;right: 0px;left: 0px;text-align: center;font-family: Tahoma;z-index: 10;background: white;margin-top: 15px;'>
 				<p><b>This plugin is actively developed and maintained by <a href='https://plus.google.com/106075761239802324012'>Jordy Meow</a></b>.<br />Please visit <a href='http://www.totorotimes.com'>Totoro Times</a>, a website about Japan, Photography, Urban Exploration & Adventures.<br />And thanks for linking Totoro Times on <a href='https://www.facebook.com/totorotimes'>Facebook</a> and <a href='https://plus.google.com/106832157268594698217'>Google+</a> :)</p>
 			</div>
-			<?
+			<?php
 		}
 	}
 ?>
